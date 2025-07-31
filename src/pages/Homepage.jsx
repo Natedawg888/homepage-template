64077cb43e0f313024cdd002c544db67eb4a93fd
NathanRobertMcCormick <McCormicknathan09@gmail.com>
2025-07-31 12:03:53 +1200

    routes set up

---
+++ src/pages/Homepage.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Homepage() {
+  return (
+    <main>
+      <h1>Welcome to My Site</h1>
+      {/* …rest of your homepage */}
+    </main>
+  );
+}
